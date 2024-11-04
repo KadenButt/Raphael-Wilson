9e7ea0b21b255f6e22db7bc3135812a25d66b35b
Musa Manshah <musamanshah04@gmail.com>
2024-11-04 10:54:21 +0000

    added register page

---
+++ resources/views/register.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width , initial-scale=1.0">
+        <title>Register</title>
+    </head>
+    <body>
+        <section id="login">
+            <div id="login-header">
+                <h2>Register</h2>
+            </div>
+            <form id="register-form" action="register.blade.php" method="POST">
+
+                <input type="email" name="email" placeholder="Email"/>
+                <br><br>
+                <input type="password" name="password" placeholder="Password"/>
+                <br><br>
+                <input type="password" name="confirm-password" placeholder="Confirm Password"/>
+                <br><br>
+                <input type="submit" name="submitted" value="Log in"/>
+
+                <input type = "hidden" name = "submitted" value = "true" />
+
+                <p>Already a user? <a href= "login.blade.php">Log in here</a></p>
+
+            </form>
+            </section>
+    </body>
+</html>
