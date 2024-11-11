93c64c3b55720f38cb24eeec49505d04d17a9fff
Musa Manshah <musamanshah04@gmail.com>
2024-11-11 13:08:18 +0000

    added favicon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width , initial-scale=1.0">
+        <link rel="icon" type = "image" href = "favicon_io\android-chrome-512x512.png">
+        
         <title>Log in</title>
     </head>
 
@@ -16,6 +18,9 @@
     </header>
 
     <body>
+
+    
+
         <section id="login">
             <div id="login-header">
                 <h2>Log in</h2>
