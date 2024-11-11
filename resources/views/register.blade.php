35f8d54f29998bde3b7ba91dce4e1051f93cf467
Musa Manshah <musamanshah04@gmail.com>
2024-11-11 13:09:19 +0000

    for register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,6 +4,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width , initial-scale=1.0">
+        <link rel="icon" type = "image" href = "favicon_io\android-chrome-512x512.png">
+        
         <title>Register</title>
     </head>
 
