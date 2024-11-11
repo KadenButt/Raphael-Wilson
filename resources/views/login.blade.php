5578d9744797b818f9688ed01216f7f9c44e0781
Musa Manshah <musamanshah04@gmail.com>
2024-11-11 12:17:56 +0000

    added navigation

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,15 @@
         <meta name="viewport" content="width=device-width , initial-scale=1.0">
         <title>Log in</title>
     </head>
+
+    <header id="navigation">
+        <button id= "home" onclick="window.location.href='index.blade.php'">Home</button>
+        <button id= "products" onclick="window.location.href='products.blade.php'">Products</button>
+        <button id= "contact" onclick="window.location.href='contact.blade.php'">Contact</button>
+        <button id= "about us" onclick="window.location.href='aboutus.blade.php'">About us</button>
+
+    </header>
+
     <body>
         <section id="login">
             <div id="login-header">
