60b76477e0647c6fa4f05fdd5baf29a8f9cee16b
Safira Gilbert <230177493@aston.ac.uk>
2024-11-19 14:52:20 +0000

    WIP - fixing broken main

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/login', function(){
-    return view('login');
-});
-
-Route::('/register', function(){
-    return view('register');
+    return view('welcome');
 });
