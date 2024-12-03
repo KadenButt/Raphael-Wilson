01720692648c985d09ab0f45490054c5a979a660
Safira Gilbert <230177493@aston.ac.uk>
2024-12-03 14:54:36 +0000

    checking out

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout Page</title>
+</head>
+<body>
+    <h1>Checkout</h1>
+
+    <!-- Order Informtion -->
+    <div>
+        <h3 id="total_price">Total Price: $0</h3>
+    </div>
+    
+    <div>
+    	<h3 id="order_number">Order Number:</h3>
+    </div>
+    
+    <div>
+    	<h3 id="order_date">Order Date:</h3>
+       	
+    
+
+    <!-- Checkout Button -->
+    <button>Return to Home</button>
+
+   
+</body>
+</html>
