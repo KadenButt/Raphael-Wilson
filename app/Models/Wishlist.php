231e297c45bdcf976d769d7353c21ffef4248e74
roheel3 <230105832@aston.ac.uk>
2025-03-24 13:59:41 +0000

    .

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -6,27 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wishlist extends Model
 {
-    // Define the table name
     protected $table = 'wishlist';
-
-    // Define the primary key
     protected $primaryKey = 'wishlist_id';
-
-    // Disable timestamps (created_at and updated_at)
     public $timestamps = false;
+    protected $fillable = ['product_id', 'customer_id'];
 
-    // Define the fillable fields
-    protected $fillable = ['user_id', 'product_id'];
-
-    // Relationship to the User model
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
-
-    // Relationship to the Product model
-    public function product()
-    {
-        return $this->belongsTo(Product::class, 'product_id');
-    }
 }
